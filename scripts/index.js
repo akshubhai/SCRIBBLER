6cc197bd7eab6858d4eb4401f162658463b64d17
akshubhai <akshubhai.reloaded@gmail.com>
2021-03-15 19:48:57 +0530

    fixess

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+//show postlist
+function showAllPosts(){
+
+}
+
+//Create post modal screen
+function showCreatePost(){
+    var createPostModal = document.getElementById("create-post-popup-modal");
+    createPostModal.style.display = "flex";
+    
+}
+
+// Closes Create Post modal window
+function hideCreatePost() {
+    var createPostModal = document.getElementById("create-post-popup-modal");
+    createPostModal.style.display = "none";
+  }
+  
